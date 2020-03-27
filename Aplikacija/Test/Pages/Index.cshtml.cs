e9b115e4475ad917b76b61c8a68185c2db5157e5
Nikola <nikola.vidanovic@elfak.rs>
2020-03-27 14:14:53 +0100

    Test projekat povezan sa MongoDb bazom

---
+++ Aplikacija/Test/Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using Test.Models;
+using MongoDB.Driver.Linq;
+
+namespace Test.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BookService bookService;
+        [BindProperty]
+        public IList<Book>Knjige {get; set;}
+        [BindProperty(SupportsGet=true)]
+        public Book NovaKnjiga{get; set;}
+
+        public IndexModel(BookService bs)
+        {
+            bookService = bs;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            var knjige = bookService.getBooks().AsQueryable<Book>();
+            Knjige = knjige.ToList();
+            
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            bookService.Create(NovaKnjiga);
+            return RedirectToPage("/Index");
+        }
+    }
+}
