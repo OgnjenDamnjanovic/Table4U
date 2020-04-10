4b6c675a2d2a4d7bb4db19e3bf1b298cc3078241
Nikola <nikola.vidanovic@elfak.rs>
2020-04-10 20:10:07 +0200

    Uradjena Top Places strana

--- Aplikacija/Table4U v1/Pages/ListedObjects.cs
+++ Aplikacija/Table4U v1/Pages/ListedObjects.cs
@@ -13,19 +13,58 @@ namespace MyApp.Namespace
     {
         public String Message {get; set;}
         private readonly Table4UContext db;
-        public string  test { get; set; }
+        public IList<String>  VrsteObjekata { get; set; }
+        public IList<List<Lokal>> MatricaLokala {get; set;}
         public ListedObjectsModel(Table4UContext dataBase)
         {
             db = dataBase;
         }
         public void OnGet(string city,string name)
         {
-            test=city+name;
+            //test=city+name;
             String eMail = HttpContext.Session.GetString("email");
             if (!string.IsNullOrEmpty(eMail))
             {
                 var korisnik = db.Korisnici.Where(x=>x.eMail == eMail).FirstOrDefault();
                 Message = "Welcome, " + korisnik.Ime;
+            }
+
+            if(string.IsNullOrEmpty(city) && string.IsNullOrEmpty(name))
+            {
+                VrsteObjekata = db.Lokali.Select(x=>x.Tip).Distinct().ToList();
+            }
+            else if(string.IsNullOrEmpty(name))
+            {
+                VrsteObjekata = db.Lokali.Where(x=>x.Grad == city).Select(x=>x.Tip).Distinct().ToList();
+            }
+            else if(string.IsNullOrEmpty(city))
+            {
+                List<Lokal> lokali = db.Lokali.Where(x=>x.Naziv == name).ToList();
+                if(lokali!=null)
+                {
+                    VrsteObjekata = lokali.Select(x=>x.Tip).Distinct().ToList();
+                    MatricaLokala = new List<List<Lokal>>(VrsteObjekata.Count());
+                    MatricaLokala.Add(lokali);
+                }
+                return;
+            }
+            else
+            {
+                Lokal lokal = db.Lokali.Where(x=>x.Naziv==name && x.Grad==city).FirstOrDefault();
+                if(lokal!=null)
+                {
+                    VrsteObjekata = new List<String>();
+                    VrsteObjekata.Add(lokal.Tip);
+                    MatricaLokala = new List<List<Lokal>>();
+                    MatricaLokala.Add(new List<Lokal>());
+                    MatricaLokala[0].Add(lokal);
+                }
+                return;
+            }
+            MatricaLokala = new List<List<Lokal>>(VrsteObjekata.Count());
+            for(int i=0; i<VrsteObjekata.Count(); i++)
+            {
+                MatricaLokala.Add(db.Lokali.Where(x=>x.Tip == VrsteObjekata[i]).ToList());                
             }
         }
 
